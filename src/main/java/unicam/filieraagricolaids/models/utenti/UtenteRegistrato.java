0ebf28150692edf42675b5dd392de20040457735
Riccardo <centini.riccardo03@gmail.com>
2025-07-08 12:41:38 +0200

    attributi User

--- src/main/java/unicam/filieraagricolaids/models/utenti/UtenteRegistrato.java
+++ src/main/java/unicam/filieraagricolaids/models/utenti/UtenteRegistrato.java
@@ -1,4 +1,17 @@
 package unicam.filieraagricolaids.models.utenti;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public abstract class UtenteRegistrato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String password;
+
 }
