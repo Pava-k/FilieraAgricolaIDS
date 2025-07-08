dbf10f75c7e8c4f604d3585046bcdf8bcf2345c0
Riccardo <centini.riccardo03@gmail.com>
2025-07-08 17:02:48 +0200

    implementazione Evento

--- src/main/java/unicam/filieraagricolaids/models/articoli/Evento.java
+++ src/main/java/unicam/filieraagricolaids/models/articoli/Evento.java
@@ -1,4 +1,88 @@
 package unicam.filieraagricolaids.models.articoli;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
 public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Date data;
+    private String luogo;
+    private String descrizione;
+    private int maxPartecipanti;
+    private int postiDisponibili;
+
+    public Evento() {}
+
+    public Evento(String nome, Date data, String luogo, String desc, int maxP) {
+        this.nome = nome;
+        this.data = data;
+        this.luogo = luogo;
+        this.descrizione = desc;
+        this.maxPartecipanti = maxP;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getLuogo() {
+        return luogo;
+    }
+
+    public void setLuogo(String luogo) {
+        this.luogo = luogo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public int getMaxPartecipanti() {
+        return maxPartecipanti;
+    }
+
+    public void setMaxPartecipanti(int maxPartecipanti) {
+        this.maxPartecipanti = maxPartecipanti;
+    }
+
+    public int getPostiDisponibili() {
+        return postiDisponibili;
+    }
+
+    public void setPostiDisponibili(int postiDisponibili) {
+        this.postiDisponibili = postiDisponibili;
+    }
 }
--- src/main/java/unicam/filieraagricolaids/models/articoli/InvitoEvento.java
+++ src/main/java/unicam/filieraagricolaids/models/articoli/InvitoEvento.java
@@ -1,4 +1,15 @@
 package unicam.filieraagricolaids.models.articoli;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class InvitoEvento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
